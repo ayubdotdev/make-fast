6b7c467bd1dfc8190d3042c9ac8e8afd2fda3f73
Ayub Khan <xyzkhn0@gmail.com>
2025-06-10 00:41:55 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
+import { Analytics } from "@vercel/analytics/next"
+
 
 export const metadata: Metadata = {
   title: 'Developer Tools Installer',
@@ -15,8 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark " >
-      
-      <body className={`${inter.className} antialiased bg-background text-foreground`}>{children}</body>
+      <body className={`${inter.className} antialiased bg-background text-foreground`}>
+        {children}
+        <Analytics/>
+        </body>
     </html>
   );
 }
